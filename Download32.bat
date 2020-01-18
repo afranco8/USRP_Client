329a3cf23e6c3a75546ba13c5a0a4fd67db91ab6
André Franco <30467158+afranco8@users.noreply.github.com>
2020-01-18 23:26:06 +0000

    Update Download32.bat
    File correction for 32bit install

--- Download32.bat
+++ Download32.bat
@@ -10,7 +10,7 @@ setlocal enabledelayedexpansion
 
 rem Download library:
 SET "FILE_URL=https://files.pythonhosted.org/packages/ba/65/ec3042de9a96e8c2eadf9bb88ddbcda434d5c679360f851a2562dd8b9942/PyAudio-0.2.11-cp27-cp27m-win32.whl"
-SET "SAVING_TO=!folder!"\PyAudio-0.2.11-cp37-cp37m-win_amd64.whl"
+SET "SAVING_TO=!folder!"\PyAudio-0.2.11-cp27-cp27m-win32.whl"
 CALL :DOWNLOAD_FILE "%FILE_URL%" "%SAVING_TO%"
 :DOWNLOAD_FILE
     rem Download command:
